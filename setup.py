25d5668a22fe5546d6e0e8373ec04b31aeff2280
TSNoble <tom.noble@bjss.com>
2021-04-11 14:14:53 +0100

    Make setup.py more genericc

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-version = "0.0.14.dev0"
+version = "0.0.1"
 here = Path(__file__).parent.resolve()
 
 with open(here.joinpath("README.md"), encoding="utf-8") as f:
@@ -11,32 +11,26 @@ with open(here.joinpath("requirements.txt"), encoding="utf-8") as f:
     requirements = f.readlines()
 
 setup(
-    name="awssert",
+    name="TODO",
     version=version,
-    description="Declarative assertions for AWS",
+    description="TODO",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/TSNoble/awssert",
-    author="Tom Noble",
-    author_email="t.s.noble@outlook.com",
+    url="TODO",
+    author="TODO",
+    author_email="TODO",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
-        "Framework :: AWS CDK",
-        "Framework :: Pytest",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
     ],
-    keywords=["aws", "python", "pytest", "boto3", "testing"],
+    keywords=["TODO"],
     packages=find_packages(),
-    python_requires=">=3.5",
+    python_requires="TODO",
     install_requires=[requirements],
-    entry_points={"pytest11": ["name_of_plugin = awssert"]},
     project_urls={
-        "Bug Reports": "https://github.com/TSNobleSoftware/awssert/issues",
-        "Source": "https://github.com/TSNobleSoftware/awssert",
+        "Bug Reports": "TODO",
+        "Source": "TODO",
     }
 )
